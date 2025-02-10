45e24c1e5ad456c746fc7a237db4ec97b76ae4ee
David Gardiner <david@gardiner.net.au>
2025-02-10 23:09:21 +1030

    Add EventHub

---
+++ event-hub.tf
@@ -0,0 +1,22 @@
+resource "azurerm_eventhub_namespace" "eh" {
+  name                = "evhns-terraform-sql-auditing-australiaeast"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  sku                 = "Standard"
+}
+
+resource "azurerm_eventhub" "eh" {
+  name              = "evh-terraform-sql-auditing-australiaeast"
+  namespace_id      = azurerm_eventhub_namespace.eh.id
+  partition_count   = 2
+  message_retention = 1
+}
+
+resource "azurerm_eventhub_namespace_authorization_rule" "eh" {
+  name                = "evhar-terraform-sql-auditing-australiaeast"
+  namespace_name      = azurerm_eventhub_namespace.eh.name
+  resource_group_name = data.azurerm_resource_group.rg.name
+  listen              = true
+  send                = true
+  manage              = true
+}
