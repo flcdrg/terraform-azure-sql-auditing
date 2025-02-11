2257d8493cb12e19de6b6bf4fd1a14f1bf76415b
David Gardiner <david@gardiner.net.au>
2025-02-11 11:24:14 +1030

    Adjust solution name

--- solution.tf
+++ solution.tf
@@ -1,5 +1,5 @@
 resource "azurerm_log_analytics_solution" "example" {
-  solution_name         = "SQLAuditing[${azurerm_log_analytics_workspace.la.name}]"
+  solution_name         = "SQLAuditing"
   location              = data.azurerm_resource_group.rg.location
   resource_group_name   = data.azurerm_resource_group.rg.name
   workspace_resource_id = azurerm_log_analytics_workspace.la.id
